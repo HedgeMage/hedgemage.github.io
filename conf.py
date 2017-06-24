5b58e0ac27da17dc7d4baebd1c3e63f558cbb54b
Susan Sons <hedgemage@binaryredneck.net>
2017-06-24 13:02:10 -0400

    github config change

--- conf.py
+++ conf.py
@@ -21,8 +21,8 @@ BLOG_AUTHOR = "Susan Sons"  # (translatable)
 BLOG_TITLE = "Susan Sons"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-#SITE_URL = "https://hedgemage.github.io/"
-SITE_URL = "http://security.engineering/"
+SITE_URL = "https://hedgemage.github.io/"
+#SITE_URL = "http://security.engineering/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "http://hedgemage.net/"
